f8cce80c9806f83adac89e74d80787a589d358bb
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-26 20:24:58 +0100

    Snack 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const num1 = parseInt(prompt("Insert a number"));
+/*const num1 = parseInt(prompt("Insert a number"));
 const num2 = parseInt(prompt("Insert another number"));
 
 if (num1 > num2){
@@ -7,4 +7,15 @@ if (num1 > num2){
     alert(num2);
 }else{
     alert("even");
+}*/
+
+const word1 = prompt("Write a word");
+const word2 = prompt("Write another word");
+
+if (word1.length > word2.length){
+    alert(word1 + " " + word2);
+} else if (word2.length > word1.length){
+    alert(word1 + " " + word2);
+}else {
+    alert("They have the same length ");
 }
