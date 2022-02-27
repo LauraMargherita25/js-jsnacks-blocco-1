b56db9f614dc3f9bd2d014075cb0032445333794
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-27 11:23:16 +0100

    Snack 3

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ if (num1 > num2){
     alert("even");
 }*/
 
-const word1 = prompt("Write a word");
+/*const word1 = prompt("Write a word");
 const word2 = prompt("Write another word");
 
 if (word1.length > word2.length){
@@ -18,4 +18,22 @@ if (word1.length > word2.length){
     alert(word1 + " " + word2);
 }else {
     alert("They have the same length ");
+}*/
+
+
+
+let arrNum = []
+
+for (let i = 0; i < 10; i++) {
+    const num = parseInt(prompt("inserisci un numero"));
+    arrNum.push(num);
 }
+
+console.log(arrNum);
+
+let somma = 0;
+for (let i = 0; i < arrNum.length; i++) {
+    somma = somma + arrNum[i];
+}
+
+alert(somma);  
